a6ce7e007926df6df0b0ff1c1542eb0881168ecf
dinesh <dineshsaravanank@gmail.com>
2014-05-31 17:23:00 +0800

    Comments and Errors

--- src/snowball.im.go
+++ src/snowball.im.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"strings"
-	//"github.com/dineshsaravanan/snowball.im/src/logger"
+	"github.com/dineshsaravanan/snowball.im/src/logger"
 )
 
 func LoginHandler(r http.ResponseWriter, req *http.Request) {
@@ -26,21 +26,29 @@ func RedirectToHome(r http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	//Create new negroni
+	n := negroni.New()
+
+	//router
 	r := mux.NewRouter()
 	r.HandleFunc("/login", LoginHandler)
 	r.HandleFunc("/home", HomeHandler)
 	r.HandleFunc("/", RedirectToHome)
 
-	n := negroni.New()
+	//use router
+	n.UseHandler(r)
+
+	//Creating static middleware, that will serve files from /public folder, we are also making sure other folders are
+	//off limits
 	static := NewStatic(http.Dir("./public"))
 	static.Prefix = "/public"
 
+	//Settimg up default middlewares
 	n.Use(negroni.NewRecovery())
 	n.Use(negroni.NewLogger())
-	//n.Use(logger.NewLogger())
-
+	n.Use(logger.NewLogger())
 	n.Use(static);
 
-	n.UseHandler(r)
+	//start listening to port 8080 for http requests
 	n.Run(":8080")
 }
